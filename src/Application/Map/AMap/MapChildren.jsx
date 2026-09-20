528c6b20626ebd82ff62811384784ee6633531e1
Devin Chen <5204106+xx7y7xx@users.noreply.github.com>
2026-09-20 21:15:43 +0800

    fix: correct thumbnail image source in MapChildren component

--- src/Application/Map/AMap/MapChildren.jsx
+++ src/Application/Map/AMap/MapChildren.jsx
@@ -46,7 +46,7 @@ const MapChildren = ({ AMap, mapInstance, container }) => {
         content={`
           <div class="hcm-photo-pin">
             <div class="hcm-photo-wrapper">
-              <img class="hcm-marker-image" src="${photo.thumbnail}1">
+              <img class="hcm-marker-image" src="${photo.thumbnail}">
               <span class="hcm-photo-count">10</span>
             </div>
           </div>
